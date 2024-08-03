7448352a88117cfe26a2186a15b26b21a8f0dac8
Tyler Keonine <tylerkeonine@gmail.com>
2024-08-03 17:10:55 -0500

    Lobby GUI Concept

--- core/src/bms/player/beatoraja/modmenu/MultiplayerMenu.java
+++ core/src/bms/player/beatoraja/modmenu/MultiplayerMenu.java
@@ -6,6 +6,8 @@ import imgui.flag.ImGuiWindowFlags;
 import imgui.type.ImBoolean;
 import imgui.type.ImString;
 
+import java.util.ArrayList;
+
 import static bms.player.beatoraja.modmenu.ImGuiRenderer.*;
 
 import bms.player.beatoraja.PlayerConfig;
@@ -14,12 +16,20 @@ public class MultiplayerMenu {
 
     public static ImBoolean MULTIPLAYER_ENABLED = new ImBoolean(false);
 
-    private static boolean inLobby = false;
+    // GUI variables
     private static String statusText = "";
     private static ImString ipInputText = new ImString(20);
     private static ImString passwordInputText = new ImString(25);
     private static boolean isTyping = false; // to be used later to disable keybinds if true
-    private static String playerName = PlayerConfig.name.substring(0, Math.min(PlayerConfig.name.length(), 20));;
+    // Client variables
+    private static boolean inLobby = false;
+    private static ArrayList<String> playerListIp = new ArrayList<String>(); 
+    // Server variables
+
+    // Player Information
+    private static String playerName = PlayerConfig.name.substring(0, Math.min(PlayerConfig.name.length(), 20));
+    private static boolean isReady = false;
+    private static boolean isHost = true;
 
     public static void show(ImBoolean showMultiplayer) {
         float relativeX = windowWidth * 0.47f;
@@ -42,21 +52,59 @@ public class MultiplayerMenu {
                 
                 if(ImGui.button("Join")) {
                     inLobby = true;
+                    isHost = false;
                 }
 
                 ImGui.sameLine();
 
                 if(ImGui.button("Host")) {
                     inLobby = true;
+                    isHost = true;
                 }
 
             }else{
                 // Lobby GUI
-                ImGui.text("LOBBY saofkjdsaklofjdsa kofjdsa  ");
-                ImGui.text("Text 2 fdasohf sdaijohfjoasidh foas ");     
+                ImGui.text("00.000.000.000:00000");
+                ImGui.sameLine();
+                ImGui.text("PasswordPasswordPasswordP");
+                ImGui.text("Artist - Song [Chart]"); // this needs to be capped
+                ImGui.text("Random: 1234567");
+                ImGui.sameLine();
+                ImGui.text("Freq: 100%");
+                
+                if (isReady){
+                    if(ImGui.button("Unready")) {
+                        isReady = false;
+                    }                    
+                }else{
+                    if(ImGui.button("Ready")) {
+                        isReady = true;
+                    }                      
+                }
+
+                ImGui.sameLine();
+
                 if(ImGui.button("Leave")) {
-                    inLobby = false;
-                }           
+                    leaveLobby();
+                }
+
+                if(isHost){
+                    ImGui.sameLine();
+                    if(ImGui.button("Transfer")) {
+                        isHost = false;
+                    }
+                    // Kick button later?
+                }
+                // for loop for each player
+                ImGui.text("Player 1");
+                ImGui.sameLine();
+                ImGui.text("(H)"); // have these hoverable to show text
+                ImGui.sameLine();
+                //  have these 2 colored gold silver etc depending on placement
+                ImGui.text("EX: 1000");
+                ImGui.sameLine();
+                ImGui.text("#1");
+                
             }
             ImGui.text(statusText); 
             ImGui.text(playerName); 
@@ -64,7 +112,28 @@ public class MultiplayerMenu {
         ImGui.end();
     }
 
+    private static void hostLobby(){ // hostLobby is different from pressing the host button. must be compatitable for pressing the host button AND being transfered host.
+        // set up server
+        
+    }
 
+    private static void joinLobby(){
+        // checks for connection
+        // if connection succeeds, have host send info
+    }
 
+    private static void leaveLobby(){
+        transferHost();
+        inLobby = false;
+        isReady = false;
+        // clear all lobby info
+        playerListIp.clear();
+    }
+
+    private static void transferHost(){
+        // check if host
+        // tell target client to hostLobby
+        // copy over info to new host
+    }
 
 }
